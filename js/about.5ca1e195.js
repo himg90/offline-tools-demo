1a4fc4243b03181089063644d7d4ad373bd06d79
Himanshu Gupta <gupta90himanshu@gmail.com>
2025-03-02 15:40:05 +0530

    v1.0

---
+++ js/about.5ca1e195.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkoffline_tools_v2"]=self["webpackChunkoffline_tools_v2"]||[]).push([[594],{5282:function(n,t,u){u.r(t),u.d(t,{default:function(){return l}});var e=u(6768);const o={class:"about"};function s(n,t){return(0,e.uX)(),(0,e.CE)("div",o,t[0]||(t[0]=[(0,e.Lk)("h1",null,"This is an about page",-1)]))}var a=u(1241);const r={},f=(0,a.A)(r,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.5ca1e195.js.map
